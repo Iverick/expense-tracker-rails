2d26741376c05101c4db4fd134c42bdf6a34f892
Pavel Goloblia <iverick091@gmail.com>
2023-06-14 00:06:45 +0300

    Add delete list modal to lists index page. Fixed db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-users = User.create([{ email: "esco@example.com", password: "123456ab" }, { email: "iverick@example.com", password: "123456ab" }, { email: "giancana@example.com", password: "123456ab" }])
-categories = Category.create([{ name: "Travelling"}, { name: "Clothing"}, { name: "Taxi"}, { name: "Cafes"}, { name: "Shops"}, { name: "Other"} ])
-lists = List.create([{ name: "Test List User 1", user_id: 1 }, { name: "Test List User 2", user_id: 2 }])
-expenses = Expense.create([
+users = User.first_or_create([{ email: "esco@example.com", password: "123456ab" }, { email: "iverick@example.com", password: "123456ab" }, { email: "giancana@example.com", password: "123456ab" }])
+categories = Category.first_or_create([{ name: "Travelling"}, { name: "Clothing"}, { name: "Taxi"}, { name: "Cafes"}, { name: "Shops"}, { name: "Other"} ])
+lists = List.first_or_create([{ name: "Test List User 1", user_id: 1 }, { name: "Test List User 2", user_id: 2 }])
+expenses = Expense.first_or_create([
   { name: "Friday taxi", amount: 1, cost: "18.3", list_id: 1, category_id: 3 },
   { name: "Visited Coffeeshop", amount: 2, cost: "2.42", list_id: 1, category_id: 4 },
   { name: "Bought T-Shirts", amount: 2, cost: "59.2", list_id: 2, category_id: 2 }
